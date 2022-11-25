a450222cad21ba10d060d1dbed5c1a2218d240f3
ueredeveloper <ueredeveloper@gmail.com>
2022-11-25 17:49:32 -0300

    melhorando explicação do serviço

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ require('dotenv').config();
 const app = express();
 
 (async function () {
+    // token ngrok - ver site da empresa
     const {NGROK_TOKEN} = process.env;
     await ngrok.authtoken(NGROK_TOKEN);
-    
+    // url tunel para localhost:80
     const url = await ngrok.connect();
+    // url de conexão com o serviço repl.it
     let { URL_SERVER } = process.env;
 
     console.log(`${url}/getPointsInPolygon`)
-
+    // enviar url tunel sempre atualizada
     const body = { url: url };
 
     const response = await fetch(`${URL_SERVER}/setLink`, {
@@ -22,6 +24,7 @@ const app = express();
         body: JSON.stringify(body),
         headers: { 'Content-Type': 'application/json' }
     });
+    // resposta do servidor apenas para confirmações
     const data = await response.json();
 
     console.log(data);
@@ -32,11 +35,11 @@ const app = express();
 const { ADASA_DATABASE, ADASA_USERNAME, ADASA_PASSWORD, ADASA_HOST } = process.env;
 
 app.get('/getPointsInPolygon', function (req, res) {
-    // mudar para post
+    // mudar para post e assim enviar um polígono para o servidor repl.it
 
     var sql = require("mssql");
 
-    // config for your database
+    // configurações do banco
     var config = {
         user: ADASA_USERNAME,
         password: ADASA_PASSWORD,
@@ -45,23 +48,19 @@ app.get('/getPointsInPolygon', function (req, res) {
         trustServerCertificate: true,
     };
 
-    // connect to your database
+    //conexão com o banco
     sql.connect(config, function (err) {
 
         if (err) console.log(err);
 
-        // create Request object
+        // criar requirisão
         var request = new sql.Request();
-
+        // polígono  que ser enviado no body
         let polygon = 'POLYGON((-47.73739483545068 -15.724018294609081,-47.50118878076318 -15.724018294609081 ,-47.502562071778804 -15.877300760051735 ,-47.74014141748193 -15.869375206281301,-47.73739483545068 -15.724018294609081))';
-
-        // query to the database and get the records
-        //request.query('select * from [gisadmin].[INTERFERENCIA] where ID_INTERFERENCIA < 2', function (err, recordset) {
+        // requisição
         request.query(`DECLARE @g geometry;SET @g = geometry::STGeomFromText('${polygon}', 4674);SELECT * FROM [SRH].[gisadmin].[INTERFERENCIA] WHERE @g.STContains(SHAPE) = 1`, function (err, recordset) {
             if (err) console.log(err)
-            // send records as a response
             res.send(JSON.stringify(recordset.recordsets));
-
         });
     });
 });
